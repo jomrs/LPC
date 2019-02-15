4b75976404478eab6e14ac817394d3911a7b8ec5
Jonhtan Mota <Jonathanmotareis@gmail.com>
2019-02-15 20:02:09 -0300

    Correção e limpeza do código.

--- Aula 01/Gerenciador_Atividades.py
+++ Aula 01/Gerenciador_Atividades.py
@@ -1,29 +1,39 @@
-class Projeto(object):
+class Projeto:
     def __init__(self, nome, data_inicio, data_fim):
         self.nome = nome
         self.data_inicio = data_inicio
         self.data_fim = data_fim
     
     def __str__(self):
-        return self.nome
+        return "Projeto: " + self.nome
 
-class Pessoa(object):
+class Pessoa:
     def __init__(self, nome, data_nascimento):
         self.nome = nome
         self.data_nascimento = data_nascimento
     
     def __str__(self):
-        return self.nome
+        return "Pessoa: " + self.nome
 
-class Atividade(object):
-    def __init__(self, nome, prioridade, nomePessoa, data_nascimento, nomeProjeto, data_inicio, data_fim):
+class Atividade:
+
+    status = "Ativo"
+    
+    def __init__(self, nome, prioridade, pessoa, projeto, data_inicio, data_fim):
         self.nome = nome
         self.prioridade = prioridade
-        self.pessoa = Pessoa(nomePessoa, data_nascimento)
-        self.projeto = Projeto(nomeProjeto, data_inicio, data_fim)
+        self.pessoa = pessoa
+        self.projeto = projeto
     
     def __str__(self):
-        return self.nome
+        return "Atividade: " + self.nome
 
-atv = Atividade("Limpar", "Elevada", "Augusto Nobrega", "1874", "Projeto limpeza", "03/02/2001", "22/02/2001")
-print(atv.projeto.nome)
+    def finalizar_atividade(self):
+        self.status = "Finalizada"
+
+
+p = Projeto("Projeto1", "15-02-2019", "31-10-2018")
+pe = Pessoa("John", "03-10-1997")
+a = Atividade("atividade1", 1, pe, p, "17-02-2019", "19-02-2019")
+
+print(a.pessoa)
