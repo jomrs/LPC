b371ddb13676630d750302aafa8d6c63d80c9e2f
Jonhtan <Jonathanmotareis@gmail.com>
2019-02-22 19:18:04 -0300

    Atualização no código

--- Aula 02/Pessoa.py
+++ Aula 02/Pessoa.py
@@ -1,8 +1,9 @@
 class Evento:
 
-    def __init__(self, nomeEvento, dataEvento):
+    def __init__(self, nomeEvento, dataEvento, dataFim):
         self.nomeEvento = nomeEvento
         self.dataEvento = dataEvento
+        self.dataFim = dataFim
 
     def __str__(self):
         return "Nome do Evento: " + self.nomeEvento + "\nData do Evento:" + self.dataEvento
@@ -19,17 +20,19 @@ class Pessoa:
 
 class Autor(Pessoa):
     
-    def __init__(self, categoria):
-        self.categoria = categoria
+    def __init__(self, curriculo, cpf, nome, data_nascimento):
+        self.curriculo = curriculo
+        super().__init__(cpf, nome, data_nascimento)
 
     def __str__(self):
         return "Nome: " + self.Pessoa.nome + "\nData Nascimento: " + self.Pessoa.dataNasc + "\nEvento:" + self.Pessoa.evento + "\nCategoria Autor:" + self.categoria
 
 class Artigo:
 
-    def __init__(self, nomeArtigo, dataArtigo):
+    def __init__(self, nomeArtigo, palavras_chave, dataArtigo):
         self.nomeArtigo = nomeArtigo
         self.dataArtigo = dataArtigo
+        self.palavras_chave = palavras_chave
 
     def __str__(self):
         return "Nome do Artigo: " + self.nomeArtigo + "\tData Artigo: " + self.dataArtigo
@@ -44,18 +47,24 @@ class ArtigoAutor:
 
 class PessoaFisica(Pessoa):
 
-    def __init__(self, cpf):
+    def __init__(self, cpf, nome, data_nascimento, evento):
         self.cpf = cpf
+        super().__init__(nome, data_nascimento, evento)
 
     def __str__(self):
         return "Nome: " + self.Pessoa.nome + "\nData Nascimento: " + self.Pessoa.dataNasc + "\nEvento:" + self.Pessoa.evento + "\nCPF:" + self.cpf
 
 class PessoaJuridica(Pessoa):
 
-    def __init__(self, cnpj):
+    def __init__(self, cnpj, nome, data_nascimento, evento):
         self.cnpj = cnpj
+        super().__init__(nome, data_nascimento, evento)
 
     def __str__(self):
         return "Nome: " + self.Pessoa.nome + "\nData Nascimento: " + self.Pessoa.dataNasc + "\nEvento:" + self.Pessoa.evento + "\nCNPJ:" + self.cnpj
+
+pf = PessoaFisica("11111", "thomas", "97", Evento("Xodrom", "03/02", "31/02"))
+print(pf.nome)
+
 
 
